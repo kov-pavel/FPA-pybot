6429a0b4e7460bd9ca3c0e1e708b2861a48166b3
Kirill Pobedonostsev <pobedonoscevkirill@gmail.com>
2022-02-11 20:23:27 +0300

    Implement function that applies a watermark

--- Bot/main.py
+++ Bot/main.py
@@ -1,11 +1,7 @@
 import telebot
-from imwatermark import WatermarkEncoder
-from imwatermark import WatermarkDecoder
-import cv2
+from watermark import apply_watermark
 
 bot = telebot.TeleBot("5129621356:AAFPoKs4SPEcU299zaJscEEXJwEgS8efxm4")
-WM = "Vasya 777"
-
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(msg):
@@ -13,45 +9,25 @@ def send_welcome(msg):
 
 
 @bot.message_handler(content_types=['document'])
-def edit_doc(doc):
+def edit_doc(message):
     try:
-        file_info = bot.get_file(doc.document.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-        bot.send_document(doc.chat.id, downloaded_file)
+        file_info = bot.get_file(message.document.file_id)
+        result = process_photo(file_info)
+        bot.send_photo(message.chat.id, result)
     except Exception as ex:
-        bot.send_message(doc.chat.id, "Error: " + ex)
-
-
-def has_wm(photo):
-    try:
-        #photo = 'test.png'
-        #photo_info = bot.get_file(photo.document.file_id)
-        #photo_in = bot.download_file(photo_info.file_path)
-        #photo_out = bot.download_file(photo_info.file_path)
-
-        bgr = cv2.imread('test.jpg')
-        wm = 'test'
-        encoder = WatermarkEncoder()
-        encoder.set_watermark('bytes', wm.encode('utf-8'))
-        bgr_encoded = encoder.encode(bgr, 'dwtDctSvd')
-        dwnld_photo = bot.download_file('test.jpg')
-        cv2.imwrite(dwnld_photo, bgr_encoded)
-        bot.send_photo(photo.chat.id, dwnld_photo)
-
-        # decoder = WatermarkDecoder('bytes', 32)
-        # watermark = decoder.decode(bgr, 'dwtDctSvd')
-        # print(watermark.decode('utf-8'))
-    except Exception as ex:
-        bot.send_message(photo.chat.id, "Error: " + ex)
-
+        print("Error: ", ex)
 
 @bot.message_handler(content_types=['photo'])
-def edit_photo(photo):
-    has_wm(photo)
-    #if has_wm(photo):
-        #print("Photo has a WM!")
-    #else:
-        #print("Photo hasn't a WM!")
+def edit_photo(message):
+    try:
+        file_info = bot.get_file(message.photo[-1].file_id)
+        result = process_photo(file_info)
+        bot.send_photo(message.chat.id, result)
+    except Exception as ex:
+        print("Error: ", ex)
 
+def process_photo(file_info) -> bytes:
+    downloaded_file = bot.download_file(file_info.file_path)
+    return apply_watermark(downloaded_file)
 
 bot.infinity_polling()
